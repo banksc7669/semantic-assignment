b8c302fd2f63b4effd1039da433d84909dd39d52
CBanks <banksc7669@yahoo.com>
2020-09-08 18:33:02 -0400

    update to format and browser semantics

--- links.js
+++ links.js
@@ -1,3 +1,17 @@
-document.getElementById ("google_translate_elementBtn").addEventListener ("Click,function ()") {
-    document.getElementById("google_translate_element").style.height= "25px"; 
-});
+var variable_one = "string"
+var variable_two = 9 //integer
+var variable_three = 9.7837 //float
+var boolean = true //false
+var array = ["an","array","is","a","collection","of","items"]
+var object = {
+    an:"object",
+    fn:function(){}
+}
+
+function spaghettiMachine(param){
+    console.log(param+1)
+}
+
+var ten = spaghettiMachine(variable_two)
+
+document.querySelector("#google_translate_element").innerText = "new thing"
